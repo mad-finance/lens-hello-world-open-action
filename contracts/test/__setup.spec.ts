ec04feb2ab8d6b516ae76354cce07b41690ec87f
benbaessler <mail@benbaessler.com>
2023-12-31 19:15:50 +0100

    Migrate test setup to v2

--- contracts/test/__setup.spec.ts
+++ contracts/test/__setup.spec.ts
@@ -5,30 +5,37 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect, use } from "chai";
 import { solidity } from "ethereum-waffle";
 import { BytesLike } from "ethers";
-import { ethers } from 'hardhat';
+import { ethers } from "hardhat";
 import {
-  Events,
-  Events__factory,
-  InteractionLogic__factory,
   LensHub,
   LensHub__factory,
-  PublishingLogic__factory,
-  ModuleGlobals__factory,
+  LensHubInitializable,
+  LensHubInitializable__factory,
   TransparentUpgradeableProxy__factory,
   FollowNFT__factory,
   CollectNFT__factory,
   FollowNFT,
   CollectNFT,
-  ProfileTokenURILogic__factory,
-  FreeCollectModule__factory,
-  Currency__factory,
-  Currency,
-  ModuleGlobals,
-  UpdatableOwnableFeeCollectModule,
-  UpdatableOwnableFeeCollectModule__factory,
+  ModuleRegistry,
+  ModuleRegistry__factory,
   TargetedCampaignReferenceModule,
   TargetedCampaignReferenceModule__factory,
-  FreeCollectModule,
+  LegacyCollectNFT,
+  TransparentUpgradeableProxy,
+  TokenHandleRegistry__factory,
+  HandleTokenURI__factory,
+  Currency__factory,
+  Currency,
+  LegacyCollectNFT__factory,
+  LensHandles__factory,
+  // ProfileTokenURILogic__factory,
+  // FreeCollectModule__factory,
+  // ModuleGlobals,
+  // UpdatableOwnableFeeCollectModule,
+  // UpdatableOwnableFeeCollectModule__factory,
+  // TargetedCampaignReferenceModule,
+  // TargetedCampaignReferenceModule__factory,
+  // FreeCollectModule,
 } from "../typechain";
 import { LensHubLibraryAddresses } from "../typechain/factories/LensHub__factory";
 import {
@@ -37,6 +44,8 @@ import {
   revertToSnapshot,
   takeSnapshot,
 } from "./helpers/utils";
+import { ZERO_ADDRESS } from "./helpers/constants";
+import { BigNumber } from "@ethersproject/bignumber";
 
 use(solidity);
 
@@ -67,8 +76,9 @@ export let chainId: number;
 export let accounts: SignerWithAddress[];
 export let deployer: SignerWithAddress;
 export let governance: SignerWithAddress;
-export let proxyAdmin: SignerWithAddress;
 export let treasury: SignerWithAddress;
+export let proxyAdmin: SignerWithAddress;
+
 export let user: SignerWithAddress;
 export let anotherUser: SignerWithAddress;
 export let thirdUser: SignerWithAddress;
@@ -83,13 +93,13 @@ export let currencyTwo: Currency;
 export let abiCoder: AbiCoder;
 export let mockModuleData: BytesLike;
 export let freeCollectModule: FreeCollectModule;
-export let followNFTImpl: FollowNFT;
-export let collectNFTImpl: CollectNFT;
+export let followNFT: FollowNFT;
+export let legacyCollectNFT: LegacyCollectNFT;
 export let hubLibs: LensHubLibraryAddresses;
-export let eventsLib: Events;
-export let moduleGlobals: ModuleGlobals;
+// export let eventsLib: Events;
+export let moduleRegistry: ModuleRegistry;
 
-export let updatableOwnableFeeCollectModule: UpdatableOwnableFeeCollectModule;
+// export let updatableOwnableFeeCollectModule: UpdatableOwnableFeeCollectModule;
 
 export let targetedCampaignReferenceModule: TargetedCampaignReferenceModule;
 
@@ -107,8 +117,9 @@ export function makeSuiteCleanRoom(name: string, tests: () => void) {
 
 beforeEach(async function () {
   chainId = Number((await ethers.provider.getNetwork()).chainId);
+  // notice: this might be an issue
   abiCoder = AbiCoder as any;
-  accounts = await ethers.getSigners() as any;
+  accounts = (await ethers.getSigners()) as any;
   deployer = accounts[0];
   governance = accounts[1];
   proxyAdmin = accounts[2];
@@ -119,76 +130,143 @@ beforeEach(async function () {
   publisher = accounts[7];
   feeRecipient = accounts[8];
 
-  // Deployment
-  moduleGlobals = await new ModuleGlobals__factory(deployer).deploy(
-    governance.address,
-    treasury.address,
-    TREASURY_FEE_BPS
-  );
-  const publishingLogic = await new PublishingLogic__factory(deployer).deploy();
-  const interactionLogic = await new InteractionLogic__factory(
-    deployer
-  ).deploy();
-  const profileTokenURILogic = await new ProfileTokenURILogic__factory(
-    deployer
-  ).deploy();
-  hubLibs = {
-    "@aave/lens-protocol/contracts/libraries/PublishingLogic.sol:PublishingLogic":
-      publishingLogic.address,
-    "@aave/lens-protocol/contracts/libraries/InteractionLogic.sol:InteractionLogic":
-      interactionLogic.address,
-    "@aave/lens-protocol/contracts/libraries/ProfileTokenURILogic.sol:ProfileTokenURILogic":
-      profileTokenURILogic.address,
-  };
-
-  // Here, we pre-compute the nonces and addresses used to deploy the contracts.
-  const nonce = await deployer.getTransactionCount();
-  // nonce + 0 is follow NFT impl
-  // nonce + 1 is collect NFT impl
-  // nonce + 2 is impl
-  // nonce + 3 is hub proxy
-
-  const hubProxyAddress = computeContractAddress(deployer.address, nonce + 3); // '0x' + keccak256(RLP.encode([deployerAddress, hubProxyNonce])).substr(26);
-
-  followNFTImpl = await new FollowNFT__factory(deployer).deploy(
-    hubProxyAddress
-  );
-  collectNFTImpl = await new CollectNFT__factory(deployer).deploy(
-    hubProxyAddress
-  );
-
-  lensHubImpl = await new LensHub__factory(hubLibs, deployer).deploy(
-    followNFTImpl.address,
-    collectNFTImpl.address
-  );
-
-  const data = lensHubImpl.interface.encodeFunctionData("initialize", [
-    LENS_HUB_NFT_NAME,
-    LENS_HUB_NFT_SYMBOL,
-    governance.address,
-  ]);
-  const proxy = await new TransparentUpgradeableProxy__factory(deployer).deploy(
-    lensHubImpl.address,
-    proxyAdmin.address,
-    data
-  );
-
-  // Connect the hub proxy to the LensHub factory and the user for ease of use.
-  lensHub = LensHub__factory.connect(proxy.address, deployer);
-
   // Currency
   currency = await new Currency__factory(deployer).deploy();
   currencyTwo = await new Currency__factory(deployer).deploy();
 
+  // Deployment
+  const moduleRegistryImpl = await new ModuleRegistry__factory(
+    deployer
+  ).deploy();
+  const moduleRegistryProxy = await new TransparentUpgradeableProxy__factory(
+    deployer
+  ).deploy(moduleRegistryImpl.address, deployer.address, "");
+
+  moduleRegistry = ModuleRegistry__factory.connect(
+    moduleRegistryProxy.address,
+    deployer
+  );
+
+  // Precompute addresses
+  const nonce = await deployer.getTransactionCount();
+
+  const followNFTImplAddr = computeContractAddress(deployer.address, nonce + 1);
+  const legacyCollectNFTImplAddr = computeContractAddress(
+    deployer.address,
+    nonce + 2
+  );
+  const hubProxyAddr = computeContractAddress(deployer.address, nonce + 3);
+  const lensHandlesImplAddr = computeContractAddress(
+    deployer.address,
+    nonce + 4
+  );
+  const lensHandlesProxyAddr = computeContractAddress(
+    deployer.address,
+    nonce + 5
+  );
+  const tokenHandleRegistryImplAddr = computeContractAddress(
+    deployer.address,
+    nonce + 6
+  );
+  const tokenHandleRegistryProxyAddr = computeContractAddress(
+    deployer.address,
+    nonce + 7
+  );
+
+  // Deploy implementation contracts
+  lensHubImpl = await new LensHubInitializable__factory(
+    hubLibs,
+    deployer
+  ).deploy(
+    followNFTImplAddr,
+    legacyCollectNFTImplAddr,
+    moduleRegistry.address,
+    // Notice: Profile guardian cooldown off
+    BigNumber.from(0),
+    {
+      lensHandlesAddress: lensHandlesProxyAddr,
+      tokenHandleRegistryAddress: tokenHandleRegistryProxyAddr,
+      legacyFeeFollowModule: ZERO_ADDRESS,
+      legacyProfileFollowModule: ZERO_ADDRESS,
+      newFeeFollowModule: ZERO_ADDRESS,
+    }
+  );
+
+  followNFT = await new FollowNFT__factory(deployer).deploy(hubProxyAddr);
+  legacyCollectNFT = await new LegacyCollectNFT__factory(deployer).deploy(
+    hubProxyAddr
+  );
+
+  // Deploy and initialize proxy
+  const lensHubProxy = await new TransparentUpgradeableProxy__factory(
+    deployer
+  ).deploy(
+    lensHubImpl.address,
+    proxyAdmin.address,
+    lensHubImpl.interface.encodeDeploy([
+      LENS_HUB_NFT_NAME,
+      LENS_HUB_NFT_SYMBOL,
+      governance.address,
+    ])
+  );
+
+  // Deploy LensHandles implementation.
+  const lensHandlesImpl = await new LensHandles__factory(deployer).deploy(
+    governance.address,
+    lensHub.address,
+    BigNumber.from(0)
+  );
+  expect(lensHandlesImpl.address).to.be.equal(lensHandlesImplAddr);
+
+  // Deploy LensHandles proxy.
+  const lensHandles = await new TransparentUpgradeableProxy__factory(
+    deployer
+  ).deploy(lensHandlesImplAddr, proxyAdmin.address, "");
+  expect(lensHandles.address).to.be.equal(lensHandlesProxyAddr);
+
+  // Deploy TokenHandleRegistry implementation.
+  const tokenHandleRegistryImpl = await new TokenHandleRegistry__factory(
+    deployer
+  ).deploy(lensHub.address, lensHandlesProxyAddr);
+  expect(tokenHandleRegistryImpl.address).to.be.equal(
+    tokenHandleRegistryImplAddr
+  );
+
+  // Deploy TokenHandleRegistry proxy.
+  const tokenHandleRegistry = await new TransparentUpgradeableProxy__factory(
+    deployer
+  ).deploy(tokenHandleRegistryImplAddr, proxyAdmin.address, "");
+  expect(tokenHandleRegistry.address).to.be.equal(tokenHandleRegistryProxyAddr);
+
+  // Cast proxy to LensHub interface.
+  lensHub = LensHub__factory.connect(lensHubProxy.address, deployer);
+
+  // TODO: Deploy URI contracts (?)
+
+  lensHub.setTreasury(treasury.address);
+  lensHub.setTreasuryFee(TREASURY_FEE_BPS);
+  // TODO: set uri contracts
+
+  // hubLibs = {
+  //   "@aave/lens-protocol/contracts/libraries/PublishingLogic.sol:PublishingLogic":
+  //     publishingLogic.address,
+  //   "@aave/lens-protocol/contracts/libraries/InteractionLogic.sol:InteractionLogic":
+  //     interactionLogic.address,
+  //   "@aave/lens-protocol/contracts/libraries/ProfileTokenURILogic.sol:ProfileTokenURILogic":
+  //     profileTokenURILogic.address,
+  // };
+
+
+  // TODO: Add tests
   // Currency whitelisting
-  await expect(
-    moduleGlobals.connect(governance).whitelistCurrency(currency.address, true)
-  ).to.not.be.reverted;
-  await expect(
-    moduleGlobals
-      .connect(governance)
-      .whitelistCurrency(currencyTwo.address, true)
-  ).to.not.be.reverted;
+  // await expect(
+  //   moduleGlobals.connect(governance).whitelistCurrency(currency.address, true)
+  // ).to.not.be.reverted;
+  // await expect(
+  //   moduleGlobals
+  //     .connect(governance)
+  //     .whitelistCurrency(currencyTwo.address, true)
+  // ).to.not.be.reverted;
 
   // Modules used for testing purposes
   freeCollectModule = await new FreeCollectModule__factory(deployer).deploy(
@@ -200,30 +278,30 @@ beforeEach(async function () {
       .whitelistCollectModule(freeCollectModule.address, true)
   ).to.not.be.reverted;
 
-  // Reference modules
-  targetedCampaignReferenceModule =
-    await new TargetedCampaignReferenceModule__factory(deployer).deploy(
-      lensHub.address,
-      moduleGlobals.address,
-      CAMPAIGN_FEE_BPS,
-      CAMPAIGN_CLIENT_FEE_BPS
-    );
-  await expect(
-    lensHub
-      .connect(governance)
-      .whitelistReferenceModule(targetedCampaignReferenceModule.address, true)
-  ).to.not.be.reverted;
+  // // Reference modules
+  // targetedCampaignReferenceModule =
+  //   await new TargetedCampaignReferenceModule__factory(deployer).deploy(
+  //     lensHub.address,
+  //     moduleGlobals.address,
+  //     CAMPAIGN_FEE_BPS,
+  //     CAMPAIGN_CLIENT_FEE_BPS
+  //   );
+  // await expect(
+  //   lensHub
+  //     .connect(governance)
+  //     .whitelistReferenceModule(targetedCampaignReferenceModule.address, true)
+  // ).to.not.be.reverted;
 
-  // Unpausing protocol
-  await expect(
-    lensHub.connect(governance).setState(ProtocolState.Unpaused)
-  ).to.not.be.reverted;
+  // // Unpausing protocol
+  // await expect(
+  //   lensHub.connect(governance).setState(ProtocolState.Unpaused)
+  // ).to.not.be.reverted;
 
-  // Profile creator whitelisting
-  await expect(
-    lensHub.connect(governance).whitelistProfileCreator(deployer.address, true)
-  ).to.not.be.reverted;
+  // // Profile creator whitelisting
+  // await expect(
+  //   lensHub.connect(governance).whitelistProfileCreator(deployer.address, true)
+  // ).to.not.be.reverted;
 
   // Event library deployment is only needed for testing and is not reproduced in the live environment
-  eventsLib = await new Events__factory(deployer).deploy();
+  // eventsLib = await new Events__factory(deployer).deploy();
 });
